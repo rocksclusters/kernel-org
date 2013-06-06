098394fd3a32eb864193098f82e24b337fe0bacc
Philip Papadopoulos <philip.papadopoulos@gmail.com>
2013-06-06 10:48:25 -0700

    Import some of rhel spec file to make a "better" kernel package. add optional support for web10G patches against the kernel.org kernel

--- src/kernel-org/version.mk
+++ src/kernel-org/version.mk
@@ -3,6 +3,13 @@ NAME        = linux
 VERSION     = 3.4.4
 RELEASE 	= 1
 TARBALL_POSTFIX	=  tar.bz2
+
+# Uncomment the following WEB10G line to indicate
+# estats Patch should be applied. Patch is kernel specific
+# WEB10G = 1
+ESTATSPATCHVER = 0.3-3.4
+ESTATSPATCH = estats-nl-$(ESTATSPATCHVER).patch
+
 DISTCONFIG  = config-$(VERSION).dist
 USERCONFIG  = config-$(VERSION)
-CONFIG	=	$(NAME)-$(VERSION)/.config
+CONFIG = 	$(NAME)-$(VERSION)/.config
